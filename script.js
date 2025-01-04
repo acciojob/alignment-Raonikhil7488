27a406f30291ee7e2f5d37be68afee0b5bddc708
Raonikhil7488 <nikhilrao7488@gmail.com>
2025-01-04 18:16:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+        <tr>
+            <td id="t31" rowspan="4"></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+           
+        </tr>
+        <tr>
